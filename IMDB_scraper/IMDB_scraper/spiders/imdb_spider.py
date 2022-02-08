5c408e4b49e5c38f53a31db39b906e96925adbfd
kellyc3 <kellyschen33@gmail.com>
2022-02-07 20:37:27 -0800

    done

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -1,5 +1,5 @@
 # to run 
-# scrapy crawl imdb_spider -o movies.csv
+# scrapy crawl imdb_spider -o results.csv
 
 import scrapy
 from scrapy.spiders import Spider
@@ -22,24 +22,21 @@ class ImdbSpider(scrapy.Spider):
 
     def parse_full_credits(self, response):
         suffixes = [a.attrib["href"] for a in response.css("td.primary_photo a")]
-        actor_urls = [self.prefix + suffix for suffix in suffixes]
+        actor_urls = ["https://www.imdb.com/" + suffix for suffix in suffixes]
         for url in actor_urls:
             yield Request(url, callback = self.parse_actor_page)
 
     def parse_actor_page(self, response):
-        film_rows = response.css("div.filmo-row")
-        credits = []
-
-        # add the name of the movie to the credits array
-        for row in film_rows:
-            id = row.css("::attr(id)").get()
-            if "actor" in id:
-                credits.append(row.css("a::text")[0].get())
-        
         # find the name of the actor
         name = response.css("h1.header").css("span::text").get()
 
-        # create and yield a dictionary d
-        d = {name : credits}
-        yield d
-
+        # go through each of the films
+        film_rows = response.css("div.filmo-row")
+        for row in film_rows:
+            id = row.css("::attr(id)").get()
+            if "actor" or "actress" in id: # determine that the actor acted in this film
+                # yield a dictionary
+                yield {
+                    "actor" : name,
+                    "movie_or_tv_name" : row.css("a::text")[0].get()
+                }
